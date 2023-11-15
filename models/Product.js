696b6780a1692eb411be313928e5a1ec99e198e0
Maxi Vecchio <maximilianovecchio@gmail.com>
2023-11-15 12:26:51 -0300

    a

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,10 @@ const ProductSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    slug: {
+        type: String,
+        required: true
+    },
     description: {
         type: String
     },
