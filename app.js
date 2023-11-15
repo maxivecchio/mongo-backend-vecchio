8d752243ff4a2797521f06b733063b532454fb30
Maxi Vecchio <maximilianovecchio@gmail.com>
2023-11-14 23:04:48 -0300

    a

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ const app = express();
 const PORT = process.env.PORT;
 
 const whitelist = [
+    'http://localhost:5173',
     'https://cambiar-moveup',
   ];
   
   // Opciones de CORS
   const corsOptions = {
     origin: function (origin, callback) {
-/*       if (whitelist.indexOf(origin) !== -1 || !origin) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
-      } */
-      callback(null, true);
+      }
     },
     // Puedes añadir más opciones si es necesario
   };
