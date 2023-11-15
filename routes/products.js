c86bc16b1b404453f880cdb61f240df77b133413
Maxi Vecchio <maximilianovecchio@gmail.com>
2023-11-15 12:24:45 -0300

    a

--- routes/products.js
+++ routes/products.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get('/', getAllProducts); 
 router.get('/:id', getProductById);
 router.post('/', createProduct);
-router.put('/:id', updateProduct);
+router.patch('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
 
 module.exports = router;
